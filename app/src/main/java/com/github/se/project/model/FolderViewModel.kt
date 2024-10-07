33b6caf8b3c2c0a7df2b96e0e8f3c9e0bc69babb
Mathieu COLLET <mathieu.collet@epfl.ch>
2024-10-07 12:12:06 +0200

    Create files for folder

---
+++ app/src/main/java/com/github/se/project/model/FolderViewModel.kt
@@ -0,0 +1,4 @@
+package com.github.se.project.model
+
+class FolderViewModel {
+}
---
+++ app/src/main/java/com/github/se/project/ui/folderScreen.kt
@@ -0,0 +1,15 @@
+package com.github.se.project.ui
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun folderScreen() {
+    Scaffold {
+        Text("Folder Screen")
+    }
+}
